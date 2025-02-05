7abd239aa60e44163c9f907f713b51a54a73179e
Ethan <ethanhuang03@gmail.com>
2025-02-05 16:31:45 -0700

    Final changes

--- a01.js
+++ a01.js
@@ -49,68 +49,105 @@ var upload = function () {
             function getMatrices() {
                 for (let i = 0; i < numDegrees; i++)  {
                     let angle = (360 / numDegrees) * i;
-                    let rotationMatrix = GetRotationMatrix(angle);
+                    let rotationMatrix = GetRotationMatrix(-angle);
                     matrices.push(rotationMatrix);
                 }
             }
-
+            
             function rotateImage() {
+                let theta = (360 / numDegrees) * currentMatrix;
+                let beta = Math.sqrt(2) * Math.cos(45 - theta % 90);
                 let newCtx = ctx.createImageData(width, height);
-                let transformationMatrix = matrices[currentMatrix];
+                let translationMatrix = GetTranslationMatrix((-width / 2) - 0.5, (-height / 2) - 0.5);
+                let scalingMatrix = GetScalingMatrix(-2 / width, 2 / height);
+                let rotationMatrix = matrices[currentMatrix];
                 currentMatrix = (currentMatrix + 1) % matrices.length;
-                
-                var maxWidth = -2147483648;
-                var minWidth = 2147483641;
-                var maxHeight = -2147483648;
-                var minHeight = 2147483641;
+                let inverseScalingMatrix = GetScalingMatrix(width / -2, height / 2);
+                let inverseTranslationMatrix = GetTranslationMatrix((width / 2) + 0.5, (height / 2) + 0.5)
+                let scale3Matrix;
 
-                for (var i = 0; i < ppm_img_data.data.length; i += 4) {
-                    var pixel = [Math.floor(i / 4) % width,
-                                 Math.floor(i / 4) / width, 1];
-            
-                    var samplePixel = MultiplyMatrixVector(transformationMatrix, pixel);
-                    maxWidth = Math.max(samplePixel[0], maxWidth);
-                    minWidth = Math.min(samplePixel[0], minWidth);
-                    maxHeight = Math.max(samplePixel[1], maxHeight);
-                    minHeight = Math.min(samplePixel[1], minHeight);
-
-                    samplePixel[0] = Math.floor(samplePixel[0]);
-                    samplePixel[1] = Math.floor(samplePixel[1]);
-            
-                    setPixelColor(newCtx, samplePixel, i);
+                if (theta % 180 != 0) {
+                    scale3Matrix = GetScalingMatrix(1 / beta, 1 / beta);
+                }
+                else {
+                    scale3Matrix = GetScalingMatrix(-1, -1);
                 }
                 
-                var widthDiff = maxWidth - minWidth;
-                var heightDiff = maxHeight - minHeight;
+                var matrix = MultiplyMatrixMatrix(inverseTranslationMatrix, inverseScalingMatrix);
+                matrix = MultiplyMatrixMatrix(matrix, scale3Matrix);
+                matrix = MultiplyMatrixMatrix(matrix, rotationMatrix);
+                matrix = MultiplyMatrixMatrix(matrix, scalingMatrix);
+                matrix = MultiplyMatrixMatrix(matrix, translationMatrix);
 
                 for (var i = 0; i < ppm_img_data.data.length; i += 4) {
                     var pixel = [Math.floor(i / 4) % width,
                         Math.floor(i / 4) / width, 1];
 
-                    var samplePixel = MultiplyMatrixVector(transformationMatrix, pixel);
-                    let widthDist = samplePixel[0] - minWidth;
-                    let heightDist = samplePixel[1] - minHeight;
-                    let widthScale = width / widthDiff;
-                    let heightScale = height / heightDiff;
-                    let scalingMatrix = GetScalingMatrix(widthScale, heightScale);                    
-
-                    samplePixel[0] = (widthDist + (widthDist / widthDiff));
-                    samplePixel[1] = (heightDist + (heightDist / heightDiff));
-                    samplePixel = MultiplyMatrixVector(scalingMatrix, samplePixel);
-
-                    // if you replace the top 3 lines with these bottom 2 lines,
-                    // the scaling is off, but the image will contain the entire bunny
-                    // with no crop out
-                    // samplePixel[0] = widthDist * (widthDist / widthDiff);
-                    // samplePixel[1] = heightDist * (heightDist / heightDiff);
-
+                    let samplePixel = MultiplyMatrixVector(matrix, pixel);
+                    
                     samplePixel[0] = Math.floor(samplePixel[0]);
                     samplePixel[1] = Math.floor(samplePixel[1]);
                     
                     setPixelColor(newCtx, samplePixel, i);
-                }
+            }
+
+            // function rotateImage() {
+            //     let newCtx = ctx.createImageData(width, height);
+            //     let transformationMatrix = matrices[currentMatrix];
+            //     currentMatrix = (currentMatrix + 1) % matrices.length;
+                
+            //     var maxWidth = -2147483648;
+            //     var minWidth = 2147483641;
+            //     var maxHeight = -2147483648;
+            //     var minHeight = 2147483641;
+
+            //     for (var i = 0; i < ppm_img_data.data.length; i += 4) {
+            //         var pixel = [Math.floor(i / 4) % width,
+            //                      Math.floor(i / 4) / width, 1];
+            
+            //         var samplePixel = MultiplyMatrixVector(transformationMatrix, pixel);
+            //         maxWidth = Math.max(samplePixel[0], maxWidth);
+            //         minWidth = Math.min(samplePixel[0], minWidth);
+            //         maxHeight = Math.max(samplePixel[1], maxHeight);
+            //         minHeight = Math.min(samplePixel[1], minHeight);
+
+            //         samplePixel[0] = Math.floor(samplePixel[0]);
+            //         samplePixel[1] = Math.floor(samplePixel[1]);
+            
+            //         setPixelColor(newCtx, samplePixel, i);
+            //     }
+                
+                // var widthDiff = maxWidth - minWidth;
+                // var heightDiff = maxHeight - minHeight;
+
+                // for (var i = 0; i < ppm_img_data.data.length; i += 4) {
+                //     var pixel = [Math.floor(i / 4) % width,
+                //         Math.floor(i / 4) / width, 1];
+
+                //     var samplePixel = MultiplyMatrixVector(transformationMatrix, pixel);
+                //     let widthDist = samplePixel[0] - minWidth;
+                //     let heightDist = samplePixel[1] - minHeight;
+                //     let widthScale = width / widthDiff;
+                //     let heightScale = height / heightDiff;
+                //     let scalingMatrix = GetScalingMatrix(widthScale, heightScale);                    
+
+                //     samplePixel[0] = (widthDist + (widthDist / widthDiff));
+                //     samplePixel[1] = (heightDist + (heightDist / heightDiff));
+                //     samplePixel = MultiplyMatrixVector(scalingMatrix, samplePixel);
+
+                //     // if you replace the top 3 lines with these bottom 2 lines,
+                //     // the scaling is off, but the image will contain the entire bunny
+                //     // with no crop out
+                //     // samplePixel[0] = widthDist * (widthDist / widthDiff);
+                //     // samplePixel[1] = heightDist * (heightDist / heightDiff);
+
+                //     samplePixel[0] = Math.floor(samplePixel[0]);
+                //     samplePixel[1] = Math.floor(samplePixel[1]);
+                    
+                //     setPixelColor(newCtx, samplePixel, i);
+                // }
                 ctx.putImageData(newCtx, canvas.width/2 - width/2, canvas.height/2 - height/2);
-                showMatrix(transformationMatrix);
+                showMatrix(matrix);
             }
             getMatrices();
             window.setInterval(rotateImage, 500);
